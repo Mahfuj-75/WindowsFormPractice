f209cf0b60fd8a68bcd178af600cd4f7dcab86a8
Mahfuj-75 <mahfuj.majumder2002@gmail.com>
2025-08-22 01:40:29 +0600

    Add project files.

---
+++ WindowsFormPractice/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.logoPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblpassrequired = new System.Windows.Forms.Label();
+            this.lblForgot = new System.Windows.Forms.Label();
+            this.lbluname = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.logoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Times New Roman", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblMessage.Location = new System.Drawing.Point(131, 263);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(208, 16);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "Sign in With your Organizational ID";
+            this.lblMessage.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Controls.Add(this.lblLogo);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(469, 51);
+            this.flowLayoutPanel2.TabIndex = 3;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblLogo.Location = new System.Drawing.Point(3, 0);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(109, 49);
+            this.lblLogo.TabIndex = 0;
+            this.lblLogo.Text = "AIUB";
+            this.lblLogo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblLogo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // logoPanel
+            // 
+            this.logoPanel.BackColor = System.Drawing.Color.White;
+            this.logoPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logoPanel.Controls.Add(this.flowLayoutPanel2);
+            this.logoPanel.Location = new System.Drawing.Point(3, 4);
+            this.logoPanel.Name = "logoPanel";
+            this.logoPanel.Size = new System.Drawing.Size(481, 61);
+            this.logoPanel.TabIndex = 2;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(57, 172);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(133, 24);
+            this.lblUser.TabIndex = 5;
+            this.lblUser.Text = "User Name :";
+            this.lblUser.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "PassWord  :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(196, 217);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(191, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnLogin.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(196, 309);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(86, 40);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblpassrequired
+            // 
+            this.lblpassrequired.AutoSize = true;
+            this.lblpassrequired.Font = new System.Drawing.Font("Times New Roman", 7.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpassrequired.ForeColor = System.Drawing.Color.Brown;
+            this.lblpassrequired.Location = new System.Drawing.Point(208, 242);
+            this.lblpassrequired.Name = "lblpassrequired";
+            this.lblpassrequired.Size = new System.Drawing.Size(0, 16);
+            this.lblpassrequired.TabIndex = 11;
+            // 
+            // lblForgot
+            // 
+            this.lblForgot.AutoSize = true;
+            this.lblForgot.Font = new System.Drawing.Font("Times New Roman", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForgot.ForeColor = System.Drawing.Color.Maroon;
+            this.lblForgot.Location = new System.Drawing.Point(329, 417);
+            this.lblForgot.Name = "lblForgot";
+            this.lblForgot.Size = new System.Drawing.Size(139, 16);
+            this.lblForgot.TabIndex = 12;
+            this.lblForgot.Text = "Forgot Your PassWord?";
+            // 
+            // lbluname
+            // 
+            this.lbluname.AutoSize = true;
+            this.lbluname.Font = new System.Drawing.Font("Times New Roman", 7.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluname.ForeColor = System.Drawing.Color.Brown;
+            this.lbluname.Location = new System.Drawing.Point(208, 195);
+            this.lbluname.Name = "lbluname";
+            this.lbluname.Size = new System.Drawing.Size(0, 16);
+            this.lbluname.TabIndex = 13;
+            this.lbluname.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(480, 442);
+            this.Controls.Add(this.lbluname);
+            this.Controls.Add(this.lblForgot);
+            this.Controls.Add(this.lblpassrequired);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.logoPanel);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
+            this.Text = "Login Page";
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.logoPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.FlowLayoutPanel logoPanel;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblpassrequired;
+        private System.Windows.Forms.Label lblForgot;
+        private System.Windows.Forms.Label lbluname;
+    }
+}
+
---
+++ WindowsFormPractice/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormPractice
+{
+    string name = "23-54475-3", pass = "12345";
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+      
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+           
+        }
+     
+    }
+}
